ef09f0597d91847cc66a2bdab9a376a15984249c
Sunila Sami <keimbyyy17@gmail.com>
2024-09-29 15:24:30 +0500

    github
    music player JS

---
+++ data.js
@@ -0,0 +1,20 @@
+let songs=[
+{
+    name:'Crystal Eyes',
+    path: 'R.mp3'    ,
+    artist: 'ROMANCOKE',
+    cover:'cover2.jpg'
+},
+{
+    name:'Right Map',
+    path: 'EDER.mp3'    ,
+    artist: 'EDER',
+    cover:'cover5.jpg'
+},
+{
+    name:'Host',
+    path: 'song3.mp3'    ,
+    artist: 'COLOR OUT',
+    cover:'i1.jpg'
+    },
+]
