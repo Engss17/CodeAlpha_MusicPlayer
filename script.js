e9c4c8504a50cf74c77ad06ce11bb0cf3f0eb035
Sunila Sami <keimbyyy17@gmail.com>
2024-10-03 14:03:39 +0500

    script.js

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-let currentMusic=0;
+let currentMusic = 0;
 
-const music=document.querySelector('#audio');
-const seekBar=document.querySelector('.seek-bar');
-const songName=document.querySelector('.musicname');
-const artistName=document.querySelector('.atname');
-const disk=document.querySelector('.disk');
-const currentTime=document.querySelector('.current-time');
-const musicDuration=document.querySelector('.song-duration');
-const playBtn=document.querySelector('.play-btn');
-const forwardBtn=document.querySelector('.forward-btn');
-const  backwardBtn=document.querySelector('.backward-btn');
+const music = document.querySelector('#audio');
+const seekBar = document.querySelector('.seek-bar');
+const songName = document.querySelector('.musicname');
+const artistName = document.querySelector('.atname');
+const disk = document.querySelector('.disk');
+const currentTime = document.querySelector('.current-time');
+const musicDuration = document.querySelector('.song-duration');
+const playBtn = document.querySelector('.play-btn');
+const forwardBtn = document.querySelector('.forward-btn');
+const backwardBtn = document.querySelector('.backward-btn');
 
-
-playBtn.addEventListener('click',()=>{
-    if(playBtn.className.includes('pause')){
+playBtn.addEventListener('click', () => {
+    if (playBtn.className.includes('pause')) {
         music.play();
-    }else{
+    } else {
         music.pause();
     }
-     playBtn.classList.toggle('pause');
-     disk.classList.toggle('play');
-})
+    playBtn.classList.toggle('pause');
+    disk.classList.toggle('play');
+});
 
-const setMusic=(i)=>{
-    seekBar.value=0;
-    let song=songs[i];
-    currentMusic=i;
-    music.src=song.path;
+const setMusic = (i) => {
+    seekBar.value = 0;
+    let song = songs[i];
+    currentMusic = i;
+    music.src = song.path;
 
-    songName.innerHTML=song.name;
-    artistName.innerHTML=song.artist;
-    disk.style.backgroundImage=`url('${song.cover}')`;
- currentTime.innerHTML='00:00';
- 
-setTimeout(() => {
-    seekBar.max = music.duration;
-    musicDuration.innerHTML = formatTime(music.duration);
-}, 300);
-}
+    songName.innerHTML = song.name;
+    artistName.innerHTML = song.artist;
+    disk.style.backgroundImage = `url('${song.cover}')`;
+    currentTime.innerHTML = '00:00';
+
+    // Listen for metadata to be loaded
+    music.addEventListener('loadedmetadata', () => {
+        seekBar.max = music.duration;
+        musicDuration.innerHTML = formatTime(music.duration);
+    }, { once: true }); 
+};
 
 setMusic(0);
 
@@ -50,45 +50,43 @@ const formatTime = (time) => {
     if (sec < 10) {
         sec = `0${sec}`;
     }
-    return `${min}: ${sec}`;
+    return `${min}:${sec}`;
 };
 
- setInterval(() => {
+setInterval(() => {
     seekBar.value = music.currentTime;
     currentTime.innerHTML = formatTime(music.currentTime);
-    if(Math.floor(music.currentTime)== Math.floor(seekBar.max)){
+    if (Math.floor(music.currentTime) === Math.floor(seekBar.max)) {
         forwardBtn.click();
     }
 }, 500);
 
 seekBar.addEventListener('change', () => {
     music.currentTime = seekBar.value;
-})
+});
 
-const playMusic=()=>{
+const playMusic = () => {
     music.play();
     playBtn.classList.remove('pause');
     disk.classList.add('play');
-}
+};
 
-forwardBtn.addEventListener('click',()=>{
-    if(currentMusic>=songs.length-1){
-        currentMusic=0;
-    }
-    else{
+forwardBtn.addEventListener('click', () => {
+    if (currentMusic >= songs.length - 1) {
+        currentMusic = 0;
+    } else {
         currentMusic++;
     }
     setMusic(currentMusic);
     playMusic();
-})
+});
 
-backwardBtn.addEventListener('click',()=>{
-    if(currentMusic <= 0){
-        currentMusic= songs.length-1;
-    }
-    else{
+backwardBtn.addEventListener('click', () => {
+    if (currentMusic <= 0) {
+        currentMusic = songs.length - 1;
+    } else {
         currentMusic--;
     }
     setMusic(currentMusic);
     playMusic();
-})
+});
